2e787b578bdc281364ca4e416835f3b7f82f0acd
Sam Eary <samwise342@gmail.com>
2026-04-07 11:19:23 -0400

    Restore unrelated changes that got removed with Input Subsystem revert

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,7 +26,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 1.5;  //4.8;/1 did nothing TODO figure out what this is/ 
+    public static final double kMaxSpeedMetersPerSecond = 4.8;  //4.8;/1 did nothing TODO figure out what this is/ 
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     // Chassis configuration
