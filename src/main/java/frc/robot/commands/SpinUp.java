467ff943ade0a71dc1337988a503c69dd9aee8b5
Sarah Eary <saraheary19@gmail.com>
2026-04-07 11:20:34 -0400

    Added min rpm supplier when camera not working

--- src/main/java/frc/robot/commands/SpinUp.java
+++ src/main/java/frc/robot/commands/SpinUp.java
@@ -22,7 +22,14 @@ public class SpinUp extends Command {
    */
   public SpinUp(CANFuelSubsystem fuelSystem, DoubleSupplier rpmSupplier) {
     this.fuelSubsystem = fuelSystem;
-    this.rpmSupplier = rpmSupplier;
+    
+    // SME20260407: If the camera isn't connected, then we end up with zero as the rpm supplier, which causes the launcher to not ramp.  stop. To prevent this, we can set a minimum RPM based on our RPM table.
+    if (rpmSupplier < kRPMTable[0])
+    {
+      rpmSupplier = kRPMTable[0]; // set to minimum as this could be that we are too close but also this is a save value. 
+    }
+    this.rpmSupplier = rpmSupplier; 
+    
     addRequirements(fuelSystem);
   }
 
