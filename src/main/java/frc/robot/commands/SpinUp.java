dfe80ae0c2dce77ab5196fe00e34fa2b4ce6eb8f
Sam Eary <samwise342@gmail.com>
2026-04-08 08:28:37 -0400

    Fix spin up implementation

--- src/main/java/frc/robot/commands/SpinUp.java
+++ src/main/java/frc/robot/commands/SpinUp.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CANFuelSubsystem;
 import static frc.robot.Constants.FuelConstants.*;
+import static frc.robot.Constants.TargetConstants.*;
 
 import java.util.function.DoubleSupplier;
 
@@ -17,17 +18,18 @@ public class SpinUp extends Command {
 
   /**
    * SpinUp command that uses a dynamic RPM.
-   * @param fuelSystem The subsystem.
+   * 
+   * @param fuelSystem  The subsystem.
    * @param rpmSupplier A supplier for the target RPM.
    */
   public SpinUp(CANFuelSubsystem fuelSystem, DoubleSupplier rpmSupplier) {
     this.fuelSubsystem = fuelSystem;
-    
-    // SME20260407: If the camera isn't connected, then we end up with zero as the rpm supplier, which causes the launcher to not ramp.  stop. To prevent this, we can set a minimum RPM based on our RPM table.
-    if (rpmSupplier < kRPMTable[0])
-    {
-      rpmSupplier = kRPMTable[0]; // set to minimum as this could be that we are too close but also this is a safe value. 
-    }
+    final double minRPM = kRPMTable[0][1];
+
+    // SME20260407: If the camera isn't connected, then we end up with zero as the
+    // rpm supplier, which causes the launcher to not ramp. stop. To prevent this,
+    // we can set a minimum RPM based on our RPM table.
+    this.rpmSupplier = () -> Math.max(rpmSupplier.getAsDouble(), minRPM);
 
     addRequirements(fuelSystem);
   }
@@ -43,7 +45,7 @@ public class SpinUp extends Command {
   public void initialize() {
     // Set launcher to target RPM
     fuelSubsystem.setLauncherRPM(rpmSupplier.getAsDouble());
-    
+
     // Set feeder roller to spin-up speed (usually negative to keep ball back)
     fuelSubsystem.setFeederRoller(INDEXER_SPIN_UP_PRE_LAUNCH_PERCENT);
   }
@@ -56,13 +58,13 @@ public class SpinUp extends Command {
 
   @Override
   public void end(boolean interrupted) {
-    // If interrupted, we don't necessarily want to stop() 
+    // If interrupted, we don't necessarily want to stop()
     // because this command is often followed by Launch
   }
 
   @Override
   public boolean isFinished() {
-    // This command is meant to be used with .withTimeout() 
+    // This command is meant to be used with .withTimeout()
     // or as part of a SequentialCommandGroup
     return false;
   }
